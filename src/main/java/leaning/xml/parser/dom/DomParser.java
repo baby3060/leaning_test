71c960c654f9d932fc8169b81c19731c7e549adf
psk <psk3060@naver.com>
2019-03-30 11:12:37 +0900

    Inner NodeList 구현

--- src/main/java/leaning/xml/parser/dom/DomParser.java
+++ src/main/java/leaning/xml/parser/dom/DomParser.java
@@ -12,7 +12,10 @@ public class DomParser {
     public void parsingTest() {
         ClassLoader classLoader = getClass().getClassLoader();
         Node node = null;
+        Node nameNode = null;
         Element element = null;
+        Element nameElement = null;
+        String nameStr = "";
         try {
             // 클래스패스에서 읽어들임.
             File xmlFile = new File(classLoader.getResource("xml_using.xml").getFile());
@@ -32,9 +35,10 @@ public class DomParser {
 
             // student 태그로 리스트를 만듦.
             NodeList nodeList = doc.getElementsByTagName("student");
-            
+            NodeList nameList = null;
             for( int listIndex = 0; listIndex < nodeList.getLength(); listIndex++ ) {
                 node = nodeList.item(listIndex);
+                nameStr = "이름 : (";
 
                 // 타입이 요소 일 경우
                 if( node.getNodeType() == Node.ELEMENT_NODE ) {
@@ -43,13 +47,29 @@ public class DomParser {
                     // getAttribute : 해당 요소의 속성을 가져옴. <student no="">
                     // getElementsByTagName : 해당 요소에 저장된 태그를 가져옴
                     // getElementsByTagName의 리턴 타입은 NodeList
-                    System.out.println("학번 : " + element.getAttribute("no") + 
-                                       ", 이름(학생으로 분류) : " + element.getElementsByTagName("name").item(0).getTextContent() + 
-                                       ", 이름(성명으로 분류) : " + element.getElementsByTagName("name").item(1).getTextContent() +
-                                       ", 수학점수 : " + element.getElementsByTagName("math").item(0).getTextContent() +
-                                       ", 영어점수 : " + element.getElementsByTagName("english").item(0).getTextContent()
+                    System.out.println("학번 : " + element.getAttribute("no"));
 
-                    );
+                    nameList = element.getElementsByTagName("name");
+                    
+                    for( int nameIndex = 0; nameIndex < nameList.getLength(); nameIndex++ ) {
+                        nameNode = nameList.item(nameIndex);
+
+                        if(nameNode.getNodeType() == Node.ELEMENT_NODE) {
+                            nameElement = (Element)nameNode;
+
+                            nameStr += nameElement.getTextContent() + ", ";
+                        }
+                    }
+
+                    nameStr = nameStr.substring(0, nameStr.lastIndexOf(","));
+
+                    nameStr += ")";
+
+                    System.out.println(nameStr);
+                    System.out.println("수학 점수 : " + element.getElementsByTagName("math").item(0).getTextContent());
+                    System.out.println("영어 점수 : " + element.getElementsByTagName("english").item(0).getTextContent());
+
+                    System.out.println("------------------------------");
                 }
             }
 
